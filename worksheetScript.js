519e8e3e19d35e5e7dece99f804eda73ce312c2d
Kaminy Doraisami <kaminydoraisami@gmail.com>
2017-01-12 23:27:31 -0600

    Removed unused functions in script file

--- worksheetScript.js
+++ worksheetScript.js
@@ -6,9 +6,9 @@ $(function() {
 	alert("this works");
    
 	var canvas = $('#myCanvas').get(0);	
-	var coordinate = {
-		x : 0,
-		y : 0
+	function Coordinate (x,y) {
+		this.x = Math.round(x);
+		this.y = Math.round(y);
 	}
 
 /*
@@ -17,6 +17,12 @@ $(function() {
 		return !isNaN(parseFloat(n)) && isFinite(n);
 	}
 */
+	$('#myCanvas').click(function(event){
+		
+		console.log("Canvas clicked");
+		validateAndWrite(event);	
+		
+	});
 	
 	function validateAndWrite(event) {
 		
@@ -35,59 +41,35 @@ $(function() {
 			$('#numberBId').focus() ;
 			return false; 
 		}
-		
-		addContent(event) ;
+		console.log("Numbers validated. Calling writeOnCanvas() ");
+		writeOnCanvas(event) ;
 		
 	}
 	
-	function addContent(event) {
+	function writeOnCanvas(event) {
 		
 		var numberA = $('#numberAId').val();
 		var numberB = $('#numberBId').val();  
 		var operator = $('#operatorId').val();
 		
 		var rect = canvas.getBoundingClientRect();	
+		console.log("The bounding rectangle coordinates are L: " + rect.left 
+			+ ", R: " + rect.right + ", T: " +rect.top +  ", B: " + rect.bottom);
+        var x = Math.round(event.clientX - rect.left);
+        var y = Math.round(event.clientY - rect.top);
+		console.log("The mouse click coordinates calculated according to canvas is " + x + " and " + y);
 
-        coordinate.x = Math.round(event.clientX - rect.left);
-        coordinate.y = Math.round(event.clientY - rect.top);
+		var clickedAt = new Coordinate(x,y);
+		
         var context = canvas.getContext("2d");
         context.font = "30pt Calibri";
         context.textAlign="right";
-        context.fillText(numberA, coordinate.x, coordinate.y);
-		context.fillText(numberB, coordinate.x, coordinate.y+30);
-		context.fillText("-----", coordinate.x, coordinate.y+50);
-		context.fillText("-----", coordinate.x, coordinate.y+75);
-		context.fillText(operator, coordinate.x+30, coordinate.y+15);	
+        context.fillText(numberA, clickedAt.x, clickedAt.y);
+		context.fillText(numberB, clickedAt.x, clickedAt.y+30);
+		context.fillText("-----", clickedAt.x, clickedAt.y+50);
+		context.fillText("-----", clickedAt.x, clickedAt.y+75);
+		context.fillText(operator,clickedAt.x+30, clickedAt.y+15);	
 	}
-
-	$('#doneId').click( function() {
-		
-		console.log("Add to Image button clicked");
-		
-		var numberA = $('#numberAId').val();
-		//TODO: explain the regex.
-		if (/\D/.test(numberA)) {
-			alert(numberA + " is not a number. Please enter a valid number for A." );
-			$('#numberAId').focus() ;
-			return false; 
-		}
-		
-		var numberB = $('#numberBId').val();    
-	 
-		if (/\D/.test(numberB)) {
-			alert(numberB + " is not a number. Please enter a valid number for B." );
-			$('#numberBId').focus() ;
-			return false; 
-		}
-	 	
-	});
-	
-	$('#myCanvas').click(function(event){
-		
-		validateAndWrite(event);
-	
-		
-	});
 
 
 });
